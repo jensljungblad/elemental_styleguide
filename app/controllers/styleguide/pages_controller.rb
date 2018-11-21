f2f8c2ba7196a01124a9bdbd1bb7983486dcba48
msalzburg <salzburg@emjot.de>
2018-11-21 16:34:48 +0100

    Include Styleguide::NavigationHelper to ensure it is available

--- app/controllers/styleguide/pages_controller.rb
+++ app/controllers/styleguide/pages_controller.rb
@@ -1,5 +1,7 @@
 module Styleguide
   class PagesController < ApplicationController
+    helper Styleguide::NavigationHelper
+
     def show
       unless params[:path]
         params[:path] = Styleguide.page_names[0][0]
