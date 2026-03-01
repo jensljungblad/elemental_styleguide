3078201d80532dca6018d48b40ccdd2d5c4d9cd9
Jens Ljungblad <jens.ljungblad@gmail.com>
2026-02-28 23:25:22 -0500

    Switch to bootstrap (#17)

--- app/helpers/elemental_styleguide/navigation_helper.rb
+++ app/helpers/elemental_styleguide/navigation_helper.rb
@@ -2,23 +2,26 @@
 
 module ElementalStyleguide
   module NavigationHelper
+    # rubocop:disable Metrics/AbcSize
     # rubocop:disable Metrics/MethodLength
-    def navigation(pages = nil, paths = [])
-      content_tag :ul do
+    def navigation(pages = nil, paths = [], level: 0)
+      content_tag :ul, class: "nav flex-column ms-#{level * 2}" do
         (pages || ElementalStyleguide.page_names).map do |page|
           path, label, children = page
           path = paths.dup << path
 
-          content_tag :li do
+          content_tag :li, class: "nav-item" do
             if children
-              [label, navigation(children, path)].join.html_safe
+              content_tag(:span, label.html_safe, class: "nav-link disabled py-1") +
+                navigation(children, path, level: level + 1)
             else
-              link_to label, elemental_styleguide.page_path(path)
+              link_to label, elemental_styleguide.page_path(path), class: "nav-link py-1"
             end
           end
         end.join.html_safe
       end
     end
     # rubocop:enable Metrics/MethodLength
+    # rubocop:enable Metrics/AbcSize
   end
 end
