ba59ed850207e83696c1251f9debf18ed75b7cbc
Jens Ljungblad <jens.ljungblad@gmail.com>
2026-02-28 23:20:53 -0500

    Switch to bootstrap

--- lib/elemental_styleguide/markdown_renderer.rb
+++ lib/elemental_styleguide/markdown_renderer.rb
@@ -4,6 +4,14 @@ module ElementalStyleguide
   class MarkdownRenderer < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
 
+    def block_quote(quote)
+      <<-BLOCKQUOTE
+        <blockquote class="border-5 border-start border-secondary-subtle ps-3">
+          #{quote}
+        </blockquote>
+      BLOCKQUOTE
+    end
+
     def block_code(code, language)
       case language
       when "example"
@@ -16,13 +24,16 @@ module ElementalStyleguide
 
     def example(code, options)
       <<-EXAMPLE
-        <div class="example">
-          <iframe src="/styleguide/example?example=#{Base64.urlsafe_encode64(code)}"
-                  width="#{options['width'] || '100%'}"
-                  height="#{options['height'] || 'auto'}"></iframe>
-        </div>
-        <div class="example-source">
-          #{block_code(code.strip, 'erb')}
+        <div class="card shadow-sm">
+          <div class="card-body">
+            <iframe src="/styleguide/example?example=#{Base64.urlsafe_encode64(code)}"
+                    width="#{options['width'] || '100%'}"
+                    height="#{options['height'] || 'auto'}"
+                    class="d-block"></iframe>
+          </div>
+          <div class="card-footer">
+            #{block_code(code.strip, 'erb')}
+          </div>
         </div>
       EXAMPLE
     end
